8ac6ba160e7bf1a23e4945e78fbcc96c6068aa78
Polina-Lelis <polina.lelis@mail.ru>
2020-04-14 21:14:28 +0300

    updated js

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 var posts = [
 
@@ -7,7 +7,9 @@ var posts = [
    description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
    createdAt: new Date('2019-03-17T23:00:00'),
    //author: 'Иванов Иван',
-   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+   photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+   hashTags: ['#world', '#happiness', '#love', '#BSU'],
+   likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
 
@@ -17,7 +19,9 @@ var posts = [
   description: 'Всё будет ок',
   createdAt: new Date('2020-03-18T23:00:00'),
   author: 'Полина Лелис',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness', '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -26,7 +30,9 @@ var posts = [
   description: 'Не ходите в универ',
   createdAt: new Date('2020-03-18T16:00:00'),
   author: 'Полина Лелис',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness','#Exadel' ,'#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -35,7 +41,9 @@ var posts = [
   description: 'Вы должны всё знать!',
   createdAt: new Date('2020-11-18T09:00:00'),
   author: 'Полина Лелис',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world','#Exadel', '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
 
@@ -45,7 +53,9 @@ var posts = [
   description: 'Музыка и танцы',
   createdAt: new Date('2020-03-19T23:00:00'),
   author: 'Кристина Бэнд',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness','#Exadel', '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
 
@@ -55,7 +65,9 @@ var posts = [
   description: 'Посмотрите на мое новые туфли и сумочку!',
   createdAt: new Date('2010-03-14T00:00:00'),
   author: 'Принцесса 007',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness', '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
 
@@ -65,7 +77,9 @@ var posts = [
   description: '57465256324163',
   createdAt: new Date('2020-03-18T23:00:00'),
   author: 'Полина Лелис',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world','#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
 
@@ -75,7 +89,9 @@ var posts = [
   description: 'Курлык',
   createdAt: new Date('2020-03-13T17:00:00'),
   author: 'Голудь Генадий',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness','#love'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
 
@@ -85,7 +101,9 @@ var posts = [
   description: 'Будьте здоровы!',
   createdAt: new Date('2005-01-13T08:00:00'),
   author: 'Полина Лелис',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world','#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -94,7 +112,9 @@ var posts = [
   description: 'Приезжайте к нам поснавать Хюгге',
   createdAt: new Date('2020-03-18T23:00:00'),
   author: 'Дания',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness','#Exadel', '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
 
@@ -104,7 +124,9 @@ var posts = [
   description: 'Только сегодня скидки на хлеб и ягоды!',
   createdAt: new Date('2050-03-18T23:00:00'),
   author: 'Евроопт',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#Exadel' ,'#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -113,7 +135,9 @@ var posts = [
   description: 'Видели новую соседку? Совсем не умеет детей воспитывать..',
   createdAt: new Date('2020-03-21T23:00:00'),
   author: 'Бабушка у подъезда',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -122,7 +146,9 @@ var posts = [
   description: 'Ты в танцах!!!!',
   createdAt: new Date('2020-10-21T20:00:00'),
   author: 'Полина Лелис',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world',  '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -131,7 +157,9 @@ var posts = [
   description: 'Более 76 тыс. человек во всем мире ',
   createdAt: new Date('2020-03-20T23:00:00'),
   author: 'Полина Лелис',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness', '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -140,7 +168,9 @@ var posts = [
   description: 'Хочу сверлить и днём, и ночью',
   createdAt: new Date('2020-03-20T01:00:00'),
   author: 'Соседка сверху',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness','#Exadel', '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -149,7 +179,9 @@ var posts = [
   description: 'Иванов',
   createdAt: new Date('2017-03-18T23:00:00'),
   author: 'Полина Лелис',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness','#Exadel', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -158,7 +190,9 @@ var posts = [
   description: 'Не забывайте надевать тёплые вещи!',
   createdAt: new Date('2020-03-19T14:00:00'),
   author: 'Весна',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness', '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -167,7 +201,9 @@ var posts = [
   description: 'Хочу быть Петровой',
   createdAt: new Date('2020-06-19T03:00:00'),
   author: 'Иванова Мария',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world',  '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -176,7 +212,9 @@ var posts = [
   description: 'спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
   createdAt: new Date('2030-03-14T22:00:00'),
   author: 'Полина Лелис',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world', '#happiness', '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
   {
@@ -185,7 +223,9 @@ var posts = [
   description: 'Любите себя сейчас',
   createdAt: new Date('2001-03-15T03:00:00'),
   author: 'Полина Лелис',
-  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+  hashTags: ['#world',  '#love', '#BSU'],
+  likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
   },
 
 ];
@@ -195,33 +235,31 @@ var posts = [
 
 
 (function() {
-
+  "use strict";
   var post = {
    id: '3',
    description: 'Just chill',
    createdAt: new Date('2020-03-15T03:28:00'),
    author: 'Полина Лелис',
-   photoLink: 'https://blog.xarta.co.uk/wp-content/uploads/2017/02/Astronaut-Chilling-On-The-Moon-With-Beer-Wallpaper-1920x1200.jpg'
+   photoLink: 'https://blog.xarta.co.uk/wp-content/uploads/2017/02/Astronaut-Chilling-On-The-Moon-With-Beer-Wallpaper-1920x1200.jpg',
+   hashTags: ['#world','#Exadel'],
+   likes: ['Лера Конфетка', 'Саша Привалов', 'Полина Филипович']
 } 
 
 
-  function getPostNumber(idElement) {
-
-    for(i = 0; i < posts.length; i++){
-
+  function getPostIndexById(idElement) {
+    for(var i = 0; i < posts.length; i++){
       if(idElement == posts[i].id){
         console.log (posts[i]);
           return i;
-          
       }
-
     }
   }
 
 
   function validatePost(post){
     if ((typeof post.id !== 'string')||(typeof post.description !== 'string')||(typeof post.createdAt !== 'object')
-      ||(typeof post.author !== 'string')) {
+      ||(typeof post.author !== 'string')||(typeof post.likes !== 'object')) {
           console.log("Invalid post");
           return false;
     }
@@ -256,6 +294,28 @@ var posts = [
       newposts = posts.filter(item => item.author === filterConfig.author);
     }
 
+    if (typeof filterConfig.hashTags === 'object') {
+          var valid = true;
+      for (let i of filterConfig.hashTags) {
+        if (typeof i !== 'string') {
+          valid = false;
+          break;
+        }
+      }
+      if (valid === true) {
+         newposts = newposts.filter(function(item){
+          var answer = true;
+          for (let i of filterConfig.hashTags) {
+            if (item.hashTags.includes(i) === false) {
+              answer = false;
+              break;
+            }
+          }
+          return answer;
+        });
+      }
+    }
+
     newposts.sort(function(firstPost, secondPost){
       return secondPost.createdAt - firstPost.createdAt; 
     })
@@ -263,7 +323,7 @@ var posts = [
     newposts = newposts.slice(skip, skip + top);
 
 
-    for(i = 0; i < newposts.length; i ++){
+    for(var i = 0; i < newposts.length; i ++){
       console.log(newposts[i]);
     }
 
@@ -273,9 +333,9 @@ var posts = [
 
 
   function editPost(idElement, post) {
-      var postId = getPostNumber(idElement);
+      var postId = getPostIndexById(idElement);
 
-      if (validatePost(posts[postId]) === false) {
+      if (validatePost(posts[postId]) !== true) {
         console.log("Invalid post");
         return false;
       }
@@ -297,6 +357,23 @@ var posts = [
         console.log("Invalid post edition! (photolink)");
         return false;
       }
+      if (typeof post.hashTags === 'object') {
+      posts[postId].hashTags = post.hashTags;
+      }
+
+      else if (typeof post.hashTags !== 'undefined') {
+        console.log("Invalid post edition! (hashTags)");
+        return false;
+      }
+
+      if (typeof post.likes === 'object') {
+        posts[postId].likes = post.likes;
+      }
+      else if (typeof post.likes !== 'undefined') {
+        console.log("Invalid post edition! (likes)");
+        return false;
+      }
+
 
       return true;
     }
@@ -318,9 +395,11 @@ var posts = [
 
   function editPost(idElement, post){
       if(validatePost(post) == true){
-        if(idElement == posts[i].id){
-           posts[i].photoLink = post.photolink;
-           console.log(posts[i]);
+        for (var i = 0; i < posts.length; i++) {
+          if(idElement == posts[i].id){
+             posts[i].photoLink = post.photolink;
+             console.log(posts[i]);
+          }
         }
 
       }
@@ -339,10 +418,10 @@ var posts = [
   console.log("GetPost testing (with id)");
   console.log("\n");
   console.log("with valid id:");
-  getPostNumber('5');
+  getPostIndexById('5');
   console.log("\n");
   console.log("with invalid id:");
-  getPostNumber(0);
+  getPostIndexById(0);
   console.log("No such post with this id!");
   console.log("\n");
 
@@ -379,6 +458,8 @@ var posts = [
   console.log("\n");
   getPosts(0, 10, {author: 'Полина Лелис'});
   console.log("\n");
-  console.log("\n");
+  console.log("AND ");
+  getPosts(0, 10, {author: 'Полина Лелис',
+                   hashTags: ['#Exadel', '#love']});
 
 }());
